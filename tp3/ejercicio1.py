5e0adbac735ee3ba80e3df6c4dfebeb3fa72ee87
Gaston Fenske <gastonfenske@MacBook-Pro-de-Gaston.local>
2022-04-03 21:38:12 -0300

    agregado el modo verboso y el hel[

--- tp3/ejercicio1.py
+++ tp3/ejercicio1.py
@@ -1,26 +1,40 @@
 import getopt, sys, os
 
 try:
-    opt,arg = getopt.getopt(sys.argv[1:], 'n:h:v:')
+    opt,arg = getopt.getopt(sys.argv[1:], 'n:hv')
 except getopt.GetoptError as error:
     print(f'Ha habido un error: {error}')
     exit()
 
+def hijo():     
+    if not os.fork():
+        suma = sum([i for i in range(os.getpid()) if i % 2 == 0])
+        if modo_verboso:
+            print("Inicio de la ejecucion del proceso hijo")
+            print(f'{os.getpid()} - {os.getppid()}: {suma}')
+            print("Fin de la ejecucion del proceso hijo")
+        else:
+            print(f'{os.getpid()} - {os.getppid()}: {suma}')
+            os._exit(0)
+
+def ayuda_uso() -> str:
+    return f"""[Para ejecutar] python3 ejercicio1.py -n **numero_hjos**
+[Modo verboso] agregar -v
+[Ayuda] agregar -h"""
+
+modo_verboso = False
 for (op,ar) in opt:
     if op == '-n':
         num_hjos = int(ar)
     elif op == '-h':
-        print("Ayuda")
+        print(ayuda_uso())
     elif op == '-v':
-        print("Modo verboso")
+        modo_verboso = True
 
-def hijo():     
-    if not os.fork():
-        suma = sum([i for i in range(os.getpid()) if i % 2 == 0])
-        print(f'{os.getpid()} - {os.getppid()}: {suma}')
-        os._exit(0)
-
-for i in range(num_hjos):
-    hijo()
+try:
+    for i in range(num_hjos):
+        hijo()
+except NameError:
+    pass
 
 
