1c140dcbcec031d57842cea5133dced2382dd3e3
Gaston Fenske <gastonfenske@MacBook-Pro-de-Gaston.local>
2022-05-05 22:56:35 -0300

    hijos liberados y padre termina

--- tp6/mmap_sig.py
+++ tp6/mmap_sig.py
@@ -1,4 +1,4 @@
-import getopt, sys, os, mmap, signal
+import getopt, sys, os, mmap, signal, time
 # try:
 #     opt,arg = getopt.getopt(sys.argv[1:], 'f:')
 #     if len(opt) != 1:
@@ -17,7 +17,10 @@ path = 'file.log'
 
 memoria = mmap.mmap(-1, 100)
 
+# finish = False
+
 def handler_padre(s, f):
+    global con
     # print(s == signal.SIGUSR1, '¿es la señal SIGUSR1?') # con esto??
     if s == signal.SIGUSR1:
         linea = memoria.readline()
@@ -25,12 +28,18 @@ def handler_padre(s, f):
         os.kill(pidh2, signal.SIGUSR1) # enviar
     if s == signal.SIGUSR2:
         print('El padre le avisar a h2 que tiene que terminar')
+        # os._exit(0)
+        con = False
         os.kill(pidh2, signal.SIGUSR2)
+        # for i in range(1):
+        #     print('YYY')
+        #     os.wait()
         # os._exit()
 
 
 
 def handler_h2(s, f):
+    global finish
     if s == signal.SIGUSR1:
         linea = memoria.readline()
         print(f'El H2 acaba de recibir la señal del padre y lee la linea: {linea.decode()}')
@@ -40,7 +49,9 @@ def handler_h2(s, f):
             archivo.flush()
     if s == signal.SIGUSR2:
         print('H2 muriendo y avisando al padre')
+        # finish = True
         os._exit(0)
+
 
 pidh1 = os.fork()
 if pidh1 == 0:
@@ -52,7 +63,7 @@ if pidh1 == 0:
         if linea == 'bye\n':
             print('H1 muriendo y avisando al padre!')
             os.kill(os.getppid(), signal.SIGUSR2)
-            # os._exit(0)
+            os._exit(0)
         else:
             print(f'H1 acaba de recibir la linea: {linea}')
             memoria.write(linea.encode('ascii'))
@@ -70,19 +81,29 @@ if pidh2 == 0:
 
     # os._exit(0)
 
+con = True
+
 # esto es el padre
 print(f'Soy el padre y mi pid es: {os.getpid()}\n')
 # signal.signal(signal.SIGUSR2, handler_padre)
+
 signal.signal(signal.SIGUSR1, handler_padre)
 signal.signal(signal.SIGUSR2, handler_padre)
-while True:
+# while True:
+#     if con:
+#         signal.pause()
+#     else:
+#         print('Entra')
+#         break
+
+while con:
     signal.pause()
+else:
+    for i in range(2):
+        print(i)
+        os.wait()
+    print('Padre espero y ahora termina')
 
-
-# else:
-#     for i in range(2):
-#         os.wait()
-# os.wait()
 
 
 
