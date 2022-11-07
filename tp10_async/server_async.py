bb2c15f3ed5cc6e1a9fbd98b7ea5f647cda9d9bd
GastonFenske <thesacha2000f@gmail.com>
2022-11-07 18:29:21 -0300

    finish remote_shell_ipv6 and remote_shell_with_asyncio

---
+++ tp10_async/server_async.py
@@ -0,0 +1,51 @@
+import asyncio, subprocess, getopt, sys
+
+try:
+    opt,arg = getopt.getopt(sys.argv[1:], 'h:p:')
+    if len(opt) != 2:
+        print("Por favor ingrese bien la cantidad de argumentos")
+        exit()
+except getopt.GetoptError as error:
+    print(f'Ha habido un error: {error}')
+    exit()
+
+for (op,ar) in opt:
+    if op == '-h':
+       host = str(ar)
+    if op == '-p':
+        port = int(ar)
+
+async def handle_echo(reader, writer):
+    while True:
+        data = await reader.read(100)
+        data = data.decode('ascii')
+        addr = writer.get_extra_info('peername')
+        if data == "bye":
+            print(f"Cliente desconectado {addr}")
+            writer.close()
+            await writer.wait_closed()
+            return
+        print(f"Recibido {data} de {addr}") 
+        command = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = command.communicate()
+        if command.returncode == 0:
+            ans = "OK \n"+ stdout
+            print(f'El comando {data} se ejecuto correctamente')
+        else:
+            ans = "ERROR \n"+ stderr
+            print(f'El comando {data} no se ejecuto correctamente')
+
+        writer.write(ans.encode('ascii'))
+        await writer.drain()
+
+async def main():
+    server = await asyncio.start_server(
+        handle_echo,
+        host,
+        port
+    )
+
+    print(f'Lanzando el server...')
+    await server.serve_forever()
+
+asyncio.run(main())
